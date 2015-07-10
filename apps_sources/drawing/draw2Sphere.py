54b8d7a401a033cc3ec72cbd1f2ea4d24377c6e0
Naegi <gouge.tristan@gmail.com>
2015-07-10 11:39:56 +0200

    Creation of a web-interface to control the globe

--- apps_sources/drawing/draw2Sphere.py
+++ apps_sources/drawing/draw2Sphere.py
@@ -6,6 +6,7 @@ import itertools,shutil
 from struct import *
 import PIL,sys,threading
 from PIL import Image,ImageEnhance
+from time import sleep
 
 import tkinter
 from tkinter.colorchooser import *
@@ -14,70 +15,59 @@ from tkinter.filedialog import askopenfilename,asksaveasfilename
 k=8
 canvas = None
 
-def rgb2hex(color):
-    def clamp(x):
-        return max(0, min(x, 255))
+def clamp(x,m):
+    return max(0, min(x, m))
 
-    return "#{0:02x}{1:02x}{2:02x}".format(clamp(color[0]), clamp(color[1]), clamp(color[2]))
+def rgb2hex(color):
+    return "#{0:02x}{1:02x}{2:02x}".format(clamp(color[0],255), clamp(color[1],255), clamp(color[2],255))
 
 
 
 class Tk2Text(threading.Thread):
     def __init__(self,gui):
         super().__init__(target=self.canvas2text,name="Canvas2Text")
-
-        self.lock = threading.Lock()
-
-        self._stop = threading.Event()
-        self._stop.clear()
-
+        self._shouldRelease = False
+        self._stop = False
         self.gui = gui
-
         self.start()
 
     def release(self):
-
-        #Need a try-Except because is the lock is already release it crashes
-        try:
-            self.lock.release()
-        except:
-            pass
+        self._shouldRelease = True
 
     def stop(self):
-        # don't totaly work
-        self._stop.set()
+        self._stop = True
         self.release()
 
     def canvas2text(self):
         x = 0
         global k
-        self.lock.acquire()
-        while not self._stop.is_set():
-            self.lock.acquire()
-
+        while True:
+            while not self._shouldRelease:
+                pass
+            if self._stop:
+                return
+            self._shouldRelease = False
             if not canvas:
                 continue
-            def clamp(x,m):
-                return max(0, min(x, m))
 
-            matrix = [["#000000" for x in range(49*k)] for x in range(100*k)]
+            matrix = [["#000000" for x in range(49*k)] for x in range(100*k)] # matrix [100][49] 
 
             #fill a rect of k*k with the color of each object
-            for id in canvas.find_overlapping(0,0,100*k,49*k):
+            for id in canvas.find_overlapping(0,0,99*k,48*k):
                 o = canvas.coords(id)
-                if len(o)>1:
-                    color = ""
-                    dx = int(o[2] - o[0])
-                    dy = int(o[3] - o[1])
-                    color =  canvas.itemcget(id, "fill")
+                if len(o)<4:
+                    raise Exception("bad Coords")
 
-                    for x in range(dx):
-                        for y in range(dy):
-                            matrix[clamp(int(o[0]) + x,100*k-1)] [clamp(int(o[1]) + y,49*k-1)] = color
+                color = ""
+                brushSize   =int((o[2] - o[0])/ k)
+                newX        =int(o[2]) 
+                newY        =int(o[1]) 
+                color =  canvas.itemcget(id, "fill")
 
-            finalMatrix = [[matrix[x*k][y*k] for y in range(49)] for x in range(100)]
-
-
+                for x in range(brushSize):
+                    for y in range(brushSize):
+                        matrix[clamp(newX+x,99*k)] [clamp(newY+y,48*k)] = color
+                finalMatrix = [[matrix[x*k][y*k] for y in range(49)] for x in range(100)]
             try:
                 f = open('sphere.colors', 'wb')
             except IOError:
@@ -85,7 +75,6 @@ class Tk2Text(threading.Thread):
                 return
 
             # begin to write into the file
-
             # the size of the img Matrix
             f.write(pack('2B',*[100,49]))
 
@@ -98,6 +87,7 @@ class Tk2Text(threading.Thread):
 
             # the rotation speed on 1 byte
             f.write(pack('B',self.gui.getRotationSpeed()))
+            f.write(pack('B',0)
             f.close()
 
     def img2text(self,filename):
@@ -122,12 +112,12 @@ class Tk2Text(threading.Thread):
             img = img.resize((100, newY), PIL.Image.ANTIALIAS)
             deltaX = 0
             maxX = 100
-            deltaY = (49 - newY)/2
+            deltaY = int((49 - newY)/2)
             maxY = newY
         else:
             newX = int(imgX/ratioY)
             img = img.resize((newX, 49), PIL.Image.ANTIALIAS)
-            deltaX = (100 - newX)/2
+            deltaX = int((100 - newX)/2)
             maxX = newX
             deltaY = 0
             maxY = 49
@@ -137,21 +127,17 @@ class Tk2Text(threading.Thread):
         matrix = [["#000000" for x in range(49)] for x in range(100)]
 
         # load the img in a matrix and convert rgb color to hex color
-        for x in range(img.size[0]):
-            for y in range(img.size[1]):
-                readX = x + deltaX
-                readY = y + deltaY
-                if readX < maxX and readY < maxY:
-                    matrix[x][y] = rgb2hex(m[readX,readY])
-
+        for x in range(maxX):
+            for y in range(maxY):
+                matrix[x + deltaX][y + deltaY] = rgb2hex(m[x,y])
         self.gui.loadSphere(matrix)
 
 class Gui:
     def __init__(self):
 
         #Canvas dimensions
-        self.canvas_height = 49*k-1
-        self.canvas_width = 100*k-1
+        self.canvas_height = 48*k
+        self.canvas_width = 99*k
 
         #initialize Main window
         self.tk2text = Tk2Text(self)
@@ -171,41 +157,99 @@ class Gui:
 
         #initialize Buttons
 
-        bouttonAskColor = tkinter.Button(self.master, text = "click to select color", command=self.selectColor)
-        bouttonSelectImg = tkinter.Button(self.master, text = "click to select an image", command=self.selectImg)
-        bouttonLoadSphere = tkinter.Button(self.master, text = "click to load a sphere", command=self.selectSphere)
-        bouttonSaveSphere = tkinter.Button(self.master, text = "click to save the drawed sphere", command=self.saveSphere)
+        self.bouttonAskColor = tkinter.Button(self.master, command=self.selectColor)
+        self.bouttonSelectImg = tkinter.Button(self.master,command=self.selectImg)
+        self.bouttonLoadSphere = tkinter.Button(self.master, command=self.selectSphere)
+        self.bouttonSaveSphere = tkinter.Button(self.master, command=self.saveSphere)
 
-        labelAskBrushSize = tkinter.Label(self.master, text="Brush size")
-        bouttonAskBrushSize = tkinter.Scale(self.master, from_= 1 , to= 50, variable=self.brushSize,orient=tkinter.HORIZONTAL)
+        self.labelAskBrushSize = tkinter.Label(self.master)
+        self.bouttonAskBrushSize = tkinter.Scale(self.master, from_= 1 , to= 50, variable=self.brushSize,orient=tkinter.HORIZONTAL)
 
-        labelAskRotationSpeed = tkinter.Label(self.master, text="Speed Rotation")
-        bouttonAskRotationSpeed = tkinter.Scale(self.master, from_= 0 , to= 40, variable=self.speed, command=self.onSpeedChange, orient=tkinter.HORIZONTAL)
+        self.labelAskRotationSpeed = tkinter.Label(self.master)
+        self.bouttonAskRotationSpeed = tkinter.Scale(self.master, from_= 0 , to= 40, variable=self.speed, command=self.onSpeedChange, orient=tkinter.HORIZONTAL)
 
-        bouttonClear = tkinter.Button(self.master, text = "Clear", command=self.clear)
+        self.bouttonClear = tkinter.Button(self.master, command=self.clear)
+       
+        self.lang = tkinter.StringVar()
+        self.lang.set("en") # initialize
 
-        self.canvas.grid(row=0,column=0,rowspan=7)
+        #Select lang
+        self.bouttonFr = tkinter.Radiobutton(self.master, text="Français", variable=self.lang, command=self.setLang, value = "fr")
+        self.bouttonEn = tkinter.Radiobutton(self.master, text="English", variable=self.lang,command=self.setLang, value = "en")
 
-        bouttonAskColor.grid(row=0,column=1,columnspan=2)
-        bouttonSelectImg.grid(row=1,column=1,columnspan=2)
-        bouttonSaveSphere.grid(row=2,column=1,columnspan=2)
-        bouttonLoadSphere.grid(row=3,column=1,columnspan=2)
+        self.bouttonExit = tkinter.Button(self.master, command=self.stop)
 
-        labelAskBrushSize.grid(row=4,column=1)
-        bouttonAskBrushSize.grid(row=4,column=2)
+        self.canvas.grid(row=0,column=0,rowspan=9)
 
-        labelAskRotationSpeed.grid(row=5,column=1)
-        bouttonAskRotationSpeed.grid(row=5,column=2)
+        self.bouttonAskColor.grid(row=0,column=1,columnspan=2)
+        self.bouttonSelectImg.grid(row=1,column=1,columnspan=2)
+        self.bouttonSaveSphere.grid(row=2,column=1,columnspan=2)
+        self.bouttonLoadSphere.grid(row=3,column=1,columnspan=2)
 
-        bouttonClear.grid(row=6,column=1, columnspan=2)
+        self.labelAskBrushSize.grid(row=4,column=1)
+        self.bouttonAskBrushSize.grid(row=4,column=2)
+
+        self.labelAskRotationSpeed.grid(row=5,column=1)
+        self.bouttonAskRotationSpeed.grid(row=5,column=2)
+
+        self.bouttonClear.grid(row=6,column=1, columnspan=2)
+        
+        self.bouttonFr.grid(row=7,column=1)
+        self.bouttonEn.grid(row=7,column=2)
+
+        self.bouttonExit.grid(row=8,column=1, columnspan=2)
 
 
         #initialize Bindings
         self.canvas.bind( "<B1-Motion>", self.paint )
-        self.master.protocol("WM_DELETE_WINDOW", self.stop)
+
+
+        self.master.wm_protocol ("WM_DELETE_WINDOW", self.stop)  # does work
 
         self.master.resizable(width=False, height=False)
+
+        self.setLang()
         tkinter.mainloop()
+
+    def setLang(self,lang=''):
+        if lang == '':
+            lang = self.lang.get()
+        languages = {
+                "fr" : {
+                    "askColor" : "Cliquez pour choisir la couleur",
+                    "selectImg" : "Cliquez pour charger une image",
+                    "loadSphere" : "Cliquez pour charger une sphère",
+                    "saveSphere" : "Cliquez pour sauvegarder l'image dessinée",
+                    "brushSize" : "Taille de la brosse",
+                    "speedRotation" : "Vitesse de rotation",
+                    "clear" : "Nettoyer la zone de dessin",
+                    "exit" : "Quitter"
+                    },
+                "en" : {
+                    "askColor" : "Click to choose the color",
+                    "selectImg" : "Click to load a picture",
+                    "loadSphere" : "Click to load a saved sphere",
+                    "saveSphere" : "Click to save the drawed sphere",
+                    "brushSize" : "Brush size",
+                    "speedRotation" : "Rotation speed",
+                    "clear" : "Clear",
+                    "exit" : "Exit"
+                    }
+                }
+
+        self.bouttonAskColor["text"]        = languages[lang]["askColor"]
+        self.bouttonSelectImg["text"]       = languages[lang]["selectImg"]
+        self.bouttonLoadSphere["text"]      = languages[lang]["loadSphere"]
+        self.bouttonSaveSphere["text"]      = languages[lang]["saveSphere"]
+        self.labelAskBrushSize["text"]      = languages[lang]["brushSize"]
+        self.labelAskRotationSpeed["text"]  = languages[lang]["speedRotation"]
+        self.bouttonClear["text"]           = languages[lang]["clear"]
+        self.bouttonExit["text"]            = languages[lang]["exit"]
+
+
+
+
+
 
     def selectSphere(self):
         filename = askopenfilename(parent=self.master,filetypes = [("Sphere's colors File",'.colors')])
@@ -228,8 +272,8 @@ class Gui:
 
     def saveSphere(self):
         #just copy the current sphere to the desired file
-
         self.tk2text.release()
+        sleep(.2)
         filename = asksaveasfilename(parent=self.master,filetypes = [("Sphere's colors File",'.colors')])
 
         if not filename:
@@ -247,7 +291,7 @@ class Gui:
             self.canvas.delete()
         for x in range(len(matrix)):
             for y in range(len(matrix[x])):
-                self._paint(x*k,y*k,matrix[x][y])
+                self._paint(x*k,y*k,matrix[x][y],False)
         self.tk2text.release()
 
     def getRotationSpeed(self):
@@ -255,14 +299,12 @@ class Gui:
 
     def selectImg(self):
         # a callback for the select Image Button
-        # this fct will open an image and draw the image to the canvas
-
+            # this fct will open an image and draw the image to the canvas
         filename = askopenfilename(parent=self.master,filetypes = [('Image File','.*')])
         self.tk2text.img2text(filename)
 
     def selectColor(self):
         # a callback for the select color button
-
         self.color = askcolor(self.color)[1]
 
     def clear(self):
@@ -271,45 +313,49 @@ class Gui:
         self.tk2text.release()
 
     def stop(self):
-        # Don't totaly work, should stop all thread and finish the program
+        # call this fct at the end of the prg
         self.tk2text.stop()
         self.master.destroy()
 
     def onSpeedChange(self, event):
+        # Not supported on the globe (software part)
         self.tk2text.release()
 
     def paint(self,event):
         # a callback on mouse click-and-move behavior
-
         x, y = event.x,event.y
         if x > self.canvas_width or y > self.canvas_height:
             return
 
         # remove objects the color is black (buggy)
         #if self.color == "#000000":
-        #    items = self.canvas.find_enclosed(x-k,y-k, x+k,y+k)
+        #    items = self.canvas.find_enclosed(x-k/2,y-k/2, x+k/2,y+k/2)
         #    for item in items:
         #        self.canvas.delete(item)
         #    self.tk2text.release()
         #    return
 
         # otherwise, create a new object
-        self._paint(x,y,self.color)
+
+        # allow to align with the 49 * 100 matrix
+        newX = int((x+1)/k) * k  
+        newY = int((y+1)/k) * k 
+        self._paint(newX,newY,self.color,True)
         self.tk2text.release()
 
     def _paint(self,x,y,color,useBrushSize = True):
         # draw a rect of k*k of center the point (x,y) and fill it with the color pass in argument
         if useBrushSize:
             rect = self.canvas.create_rectangle(
-                int(x- k/2 - k*self.brushSize.get()/2),
-                int(y - k/2 - k*self.brushSize.get()/2),
-                int(x + k/2 + k*self.brushSize.get()/2),
-                int(y + k/2 + k*self.brushSize.get()/2),
+                int((x - int(k * self.brushSize.get()/2)) / k) * k,
+                int((y - int(k * self.brushSize.get()/2)) / k) * k,
+                int((x + int(k * self.brushSize.get()/2)) / k) * k,
+                int((y + int(k * self.brushSize.get()/2)) / k) * k,
                 fill=color, width=0)
 
         else:
             rect = self.canvas.create_rectangle(
-                int(x- k/2),
+                int(x - k/2),
                 int(y - k/2),
                 int(x + k/2),
                 int(y + k/2),
